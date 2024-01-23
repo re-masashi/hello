087c5677283a3504e95439c402c93cfbb6af105c
Nafi <nafines007@gmail.com>
2024-01-23 19:58:53 +0530

    releases configuration

--- config/releases.exs
+++ config/releases.exs
@@ -1,5 +1,5 @@
 import Config
 config :hello, HelloWeb.Endpoint,
   server: true,
-  http: [port: {:system, "PORT"}, ip: {127, 0, 0, 1}], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
+  http: [port: {:system, "PORT"}], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
